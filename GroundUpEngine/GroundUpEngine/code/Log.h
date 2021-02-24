9cc42a6b6d3c1174a107c96bf0e9794a267f086f
Ethan Hoppe <32722553+EthanTheHuman@users.noreply.github.com>
2021-02-24 15:16:55 +1300

    New log functions

--- GroundUpEngine/GroundUpEngine/code/Log.h
+++ GroundUpEngine/GroundUpEngine/code/Log.h
@@ -14,5 +14,9 @@ public:
 	enum logMode { LOG = 0, WARNING, ERROR, IMPORTANT};
 	static logMode filter;
 	static void log(std::string _input, logMode _mode = logMode::LOG);
+	static void log(std::string _input);
+	static void warn(std::string _input);
+	static void error(std::string _input) { log(_input, logMode::ERROR); };
+	static void important(std::string _input) { log(_input, logMode::IMPORTANT); };
 };
 
